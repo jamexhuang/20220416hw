3cb82c0422d8ac80ad448e184126b6191d9d00cf
James Huang <james@mcsh.co>
2022-04-17 13:06:26 +0000

    改了一些字

--- hw.py
+++ hw.py
@@ -3,15 +3,14 @@
 #pay: 實付款
 
 print("企管1, 311057012, 黃子峻")
-amount = int(input('請輸入購物金額:'))
+amount = int(input('請輸入購物金額: '))
 
 if amount < 5000:
     pay = int(amount * 95/100)
     discount = str(amount - pay)
     print("折扣數為: 95 折")
     print("折扣金額為:" + discount)
-    print(pay)
-    print(discount)
+    print("實付金額為:" + pay)
 elif amount < 8000:
     print(amount * 90/100)
 elif amount < 10000:
