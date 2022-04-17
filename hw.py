153d7744bcd8e31ac00647af67ff3077ebf024c9
James Huang <james@mcsh.co>
2022-04-17 16:53:03 +0000

    remove round amount - pay

--- hw.py
+++ hw.py
@@ -11,46 +11,47 @@ try:
 
     if amount < 0:
         print("金額不可小於0\n程式結束")
+        exit(0)
 
     elif amount < 5000:
         #打95折
         pay = round(amount * 95/100,1)
-        discount_amount = round(amount - pay,1)
+        discount_amount = amount - pay
         print("折扣數為: 95 折")
         print("折扣金額為: " + str(discount_amount) + "\n實付金額為: " + str(pay))
 
     elif amount < 8000:
         #打9折
         pay = round(amount * 90/100,1)
-        discount_amount = round(amount - pay,1)
+        discount_amount = amount - pay
         print("折扣數為: 9 折")
         print("折扣金額為: " + str(discount_amount) + "\n實付金額為: " + str(pay))
 
     elif amount < 10000:
         #打85折
         pay = round(amount * 85/100,1)
-        discount_amount = round(amount - pay,1)
+        discount_amount = amount - pay
         print("折扣數為: 85 折")
         print("折扣金額為: " + str(discount_amount) + "\n實付金額為: " + str(pay))
 
     elif amount < 20000:
         #打8折
         pay = round(amount * 80/100,1)
-        discount_amount = round(amount - pay,1)
+        discount_amount = amount - pay
         print("折扣數為: 8 折")
         print("折扣金額為: " + str(discount_amount) + "\n實付金額為: " + str(pay))
 
     elif amount < 30000:
         #打75折
         pay = round(amount * 75/100,1)
-        discount_amount = round(amount - pay,1)
+        discount_amount = amount - pay
         print("折扣數為: 75 折")
         print("折扣金額為: " + str(discount_amount) + "\n實付金額為: " + str(pay))
 
     else:
         #打7折
         pay = round(amount * 70/100,1)
-        discount_amount = round(amount - pay,1)
+        discount_amount = amount - pay
         print("折扣數為: 7 折")
         print("折扣金額為: " + str(discount_amount) + "\n實付金額為: " + str(pay))
 
